ad97364288f0154a5cc5132bcceb05c6141a664f
taskin8651 <mdsayebalam10@gmail.com>
2025-11-01 16:51:37 +0530

    ssd

--- resources/views/admin/cashInHands/index.blade.php
+++ resources/views/admin/cashInHands/index.blade.php
@@ -1,158 +1,106 @@
 @extends('layouts.admin')
+
 @section('content')
-<div class="content">
-    @can('cash_in_hand_create')
-        <div style="margin-bottom: 10px;" class="row">
-            <div class="col-lg-12">
-                <a class="btn btn-success" href="{{ route('admin.cash-in-hands.create') }}">
-                    {{ trans('global.add') }} {{ trans('cruds.cashInHand.title_singular') }}
-                </a>
-                <button class="btn btn-warning" data-toggle="modal" data-target="#csvImportModal">
-                    {{ trans('global.app_csvImport') }}
-                </button>
-                @include('csvImport.modal', ['model' => 'CashInHand', 'route' => 'admin.cash-in-hands.parseCsvImport'])
+<div class="p-6 bg-gray-100 min-h-screen">
+    <div class="max-w-7xl mx-auto">
+        <div class="flex items-center justify-between mb-6">
+            <h2 class="text-2xl font-bold text-indigo-700 flex items-center gap-2">
+                <i class="fas fa-wallet text-indigo-600"></i>
+                {{ trans('cruds.cashInHand.title_singular') }} {{ trans('global.list') }}
+            </h2>
+
+            <div class="flex items-center gap-3">
+                @can('cash_in_hand_create')
+                    <a href="{{ route('admin.cash-in-hands.create') }}"
+                       class="px-4 py-2 bg-indigo-600 text-white font-semibold rounded hover:bg-indigo-700 transition">
+                        <i class="fas fa-plus mr-1"></i> {{ trans('global.add') }}
+                    </a>
+                    <button class="px-4 py-2 bg-yellow-500 text-white font-semibold rounded hover:bg-yellow-600 transition"
+                            data-toggle="modal" data-target="#csvImportModal">
+                        <i class="fas fa-file-csv mr-1"></i> {{ trans('global.app_csvImport') }}
+                    </button>
+                @endcan
             </div>
         </div>
-    @endcan
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    {{ trans('cruds.cashInHand.title_singular') }} {{ trans('global.list') }}
-                </div>
-                <div class="panel-body">
-                    <div class="table-responsive">
-                        <table class=" table table-bordered table-striped table-hover datatable datatable-CashInHand">
-                            <thead>
-                                <tr>
-                                    <th width="10">
 
-                                    </th>
-                                    <th>
-                                        {{ trans('cruds.cashInHand.fields.id') }}
-                                    </th>
-                                    <th>
-                                        {{ trans('cruds.cashInHand.fields.adjustment') }}
-                                    </th>
-                                    <th>
-                                        {{ trans('cruds.cashInHand.fields.enter_amount') }}
-                                    </th>
-                                    <th>
-                                        {{ trans('cruds.cashInHand.fields.adjustment_date') }}
-                                    </th>
-                                    <th>
-                                        Account Number
-                                    </th>
-                                    <th>
-                                        &nbsp;
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($cashInHands as $key => $cashInHand)
-                                    <tr data-entry-id="{{ $cashInHand->id }}">
-                                        <td>
+        @include('csvImport.modal', ['model' => 'CashInHand', 'route' => 'admin.cash-in-hands.parseCsvImport'])
 
-                                        </td>
-                                        <td>
-                                            {{ $cashInHand->id ?? '' }}
-                                        </td>
-                                        <td>
-                                            {{ $cashInHand->account_name }}
-                                        </td>
-                                        <td>
-                                            {{ $cashInHand->opening_balance ?? '' }}
-                                        </td>
-                                        <td>
-                                            {{ $cashInHand->as_of_date ?? '' }}
-                                        </td>
-                                        <td>
-                                            {{ $cashInHand->account_number ?? '' }}
-                                        </td>
-                                        <td>
-                                            @can('cash_in_hand_show')
-                                                <a class="btn btn-xs btn-primary" href="{{ route('admin.cash-in-hands.show', $cashInHand->id) }}">
-                                                    {{ trans('global.view') }}
-                                                </a>
-                                            @endcan
-
-                                            @can('cash_in_hand_edit')
-                                                <a class="btn btn-xs btn-info" href="{{ route('admin.cash-in-hands.edit', $cashInHand->id) }}">
-                                                    {{ trans('global.edit') }}
-                                                </a>
-                                            @endcan
-
-                                            @can('cash_in_hand_delete')
-                                                <form action="{{ route('admin.cash-in-hands.destroy', $cashInHand->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
-                                                    <input type="hidden" name="_method" value="DELETE">
-                                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                                    <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
-                                                </form>
-                                            @endcan
-
-                                        </td>
-
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+        <div class="bg-white shadow rounded-lg overflow-hidden p-2">
+            <div class="overflow-x-auto">
+                <table class="min-w-full divide-y divide-gray-200" id="cashInHandTable">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase">#</th>
+                            <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                                {{ trans('cruds.cashInHand.fields.id') }}
+                            </th>
+                            <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                                {{ trans('cruds.cashInHand.fields.adjustment') }}
+                            </th>
+                            <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                                {{ trans('cruds.cashInHand.fields.enter_amount') }}
+                            </th>
+                            <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                                {{ trans('cruds.cashInHand.fields.adjustment_date') }}
+                            </th>
+                            <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase">Account Number</th>
+                            <th class="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-100">
+                        @foreach($cashInHands as $key => $cashInHand)
+                            <tr class="hover:bg-gray-50 transition" data-entry-id="{{ $cashInHand->id }}">
+                                <td class="px-4 py-3 text-sm text-gray-700">{{ $loop->iteration }}</td>
+                                <td class="px-4 py-3 text-sm text-gray-700">{{ $cashInHand->id ?? '' }}</td>
+                                <td class="px-4 py-3 text-sm text-gray-700">{{ $cashInHand->account_name ?? '' }}</td>
+                                <td class="px-4 py-3 text-sm text-gray-700">{{ $cashInHand->opening_balance ?? '' }}</td>
+                                <td class="px-4 py-3 text-sm text-gray-700">{{ $cashInHand->as_of_date ?? '' }}</td>
+                                <td class="px-4 py-3 text-sm text-gray-700">{{ $cashInHand->account_number ?? '' }}</td>
+                                <td class="px-4 py-3 text-sm text-center">
+                                    <div class="flex justify-center gap-2">
+                                        @can('cash_in_hand_show')
+                                            <a href="{{ route('admin.cash-in-hands.show', $cashInHand->id) }}"
+                                               class="px-3 py-1 bg-green-500 text-white text-xs rounded hover:bg-green-600 transition">
+                                                <i class="fas fa-eye"></i>
+                                            </a>
+                                        @endcan
+                                        @can('cash_in_hand_edit')
+                                            <a href="{{ route('admin.cash-in-hands.edit', $cashInHand->id) }}"
+                                               class="px-3 py-1 bg-blue-500 text-white text-xs rounded hover:bg-blue-600 transition">
+                                                <i class="fas fa-edit"></i>
+                                            </a>
+                                        @endcan
+                                        @can('cash_in_hand_delete')
+                                            <form action="{{ route('admin.cash-in-hands.destroy', $cashInHand->id) }}" method="POST" 
+                                                  onsubmit="return confirm('{{ trans('global.areYouSure') }}');" class="inline">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit"
+                                                        class="px-3 py-1 bg-red-500 text-white text-xs rounded hover:bg-red-600 transition">
+                                                    <i class="fas fa-trash"></i>
+                                                </button>
+                                            </form>
+                                        @endcan
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
-
-
-
         </div>
     </div>
 </div>
 @endsection
+
 @section('scripts')
 @parent
 <script>
-    $(function () {
-  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
-@can('cash_in_hand_delete')
-  let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
-  let deleteButton = {
-    text: deleteButtonTrans,
-    url: "{{ route('admin.cash-in-hands.massDestroy') }}",
-    className: 'btn-danger',
-    action: function (e, dt, node, config) {
-      var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
-          return $(entry).data('entry-id')
-      });
-
-      if (ids.length === 0) {
-        alert('{{ trans('global.datatables.zero_selected') }}')
-
-        return
-      }
-
-      if (confirm('{{ trans('global.areYouSure') }}')) {
-        $.ajax({
-          headers: {'x-csrf-token': _token},
-          method: 'POST',
-          url: config.url,
-          data: { ids: ids, _method: 'DELETE' }})
-          .done(function () { location.reload() })
-      }
-    }
-  }
-  dtButtons.push(deleteButton)
-@endcan
-
-  $.extend(true, $.fn.dataTable.defaults, {
-    orderCellsTop: true,
-    order: [[ 1, 'desc' ]],
-    pageLength: 100,
-  });
-  let table = $('.datatable-CashInHand:not(.ajaxTable)').DataTable({ buttons: dtButtons })
-  $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e){
-      $($.fn.dataTable.tables(true)).DataTable()
-          .columns.adjust();
-  });
-  
-})
-
+$(function () {
+    let table = $('#cashInHandTable').DataTable({
+        order: [[1, 'desc']],
+        pageLength: 25,
+    });
+});
 </script>
 @endsection
